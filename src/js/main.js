7af71190da84492b3c0d591d9d98a106906813e0
Gerald Michelant <gerald.michelant@gmail.com>
2017-11-24 00:08:50 -0500

    final project

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,38 @@
-workDuration= document.getElementById('workDuration');
-restDuration= document.getElementById('restDuration');
-wdProgress= document.getElementById('wdProgress');
-rdProgress= document.getElementById('rdProgress');
-sessionProgress= document.getElementById('sessionProgress');
-var cdTimer;
-var w,r,s;
+var workDuration= document.getElementById('workDuration');
+var restDuration= document.getElementById('restDuration');
+var wdProgress= document.getElementById('wdProgress');
+var rdProgress= document.getElementById('rdProgress');
+var sessionProgress= document.getElementById('sessionProgress');
+var cdTimer,cdTimerConst, countDownDate;
+var hours, minutes, seconds;
+var w, wt =0;
+var r, rt = 0;
+var s, st=0; 
+var duree=0;
 var x;
 var started=false;
 var distance, now;
-reset();
-adjustSessionDuration() 
+adjustSessionDuration();
 
 function minusWD(){
-//  if (Number(workDuration.innerHTML) <5) {
-//    document.getElementsByClassName('alert')[0].classList.remove("hidden");
-//    setTimeout(function(){ document.getElementsByClassName('alert')[0].classList.add("hidden") }, 2000);
-//  }
- // else {
+  if (Number(workDuration.innerHTML) <5) {
+    document.getElementsByClassName('alert')[0].classList.remove("hidden");
+    setTimeout(function(){ document.getElementsByClassName('alert')[0].classList.add("hidden") }, 2000);
+  }
+  else {
     workDuration.innerHTML= Number(workDuration.innerHTML)-1;
     adjustSessionDuration();
-//  }
+  }
 };
 function plusWD(){
+    if (Number(workDuration.innerHTML) >=90) {
+    document.getElementsByClassName('alert')[0].classList.remove("hidden");
+    setTimeout(function(){ document.getElementsByClassName('alert')[0].classList.add("hidden") }, 2000);
+  }
+  else {
   workDuration.innerHTML= Number(workDuration.innerHTML)+1;
   adjustSessionDuration();
+}
 };
 function minusRD(){
   if (Number(restDuration.innerHTML) <3) {
@@ -36,29 +45,37 @@ function minusRD(){
   }
 };
 function plusRD(){
+   if (Number(restDuration.innerHTML) >=30) {
+    document.getElementsByClassName('alert')[0].classList.remove("hidden");
+    setTimeout(function(){ document.getElementsByClassName('alert')[0].classList.add("hidden") }, 2000);
+  }
+  else {
   restDuration.innerHTML= Number(restDuration.innerHTML)+1;
   adjustSessionDuration();
+}
 };
 
 function adjustSessionDuration() {
   cdTimer= (Number(workDuration.innerHTML) + Number(restDuration.innerHTML))*60000;
+  cdTimerConst=cdTimer;
+  s=0;
   countDownDate = new Date();
   countDownDate.setMilliseconds(countDownDate.getMilliseconds()+ cdTimer);
   r = (Number(restDuration.innerHTML))*100/(cdTimer/60000);
   w = (Number(workDuration.innerHTML))*100/(cdTimer/60000);
-  wdProgress.innerHTML=workDuration.innerHTML + " m";
-  rdProgress.innerHTML=restDuration.innerHTML + " m";
+  rt=restDuration.innerHTML + " m";
+  CalcTime();
   afficher();
-  s=1/cdTimer*100000;
 };
 
 function startStop(){
   if (started==false){ 
+    duree=1/cdTimerConst*100000;
     countDownDate = new Date();
     countDownDate.setMilliseconds(countDownDate.getMilliseconds()+ cdTimer);
     started=true;
     var list = document.getElementsByClassName('colT');
-    for (i=0; i<list.length; i++) {
+    for (var i=0; i<list.length; i++) {
       list[i].setAttribute('class', 'col colT disabled');
     }
     document.getElementsByClassName('btn')[0].innerHTML="Stop";
@@ -70,7 +87,7 @@ function startStop(){
     cdTimer=distance;
     clearInterval(x);
     var list=document.getElementsByClassName('colT');
-    for (i=0; i<list.length; i++) {
+    for (var i=0; i<list.length; i++) {
       list[i].setAttribute('class', 'col colT');
     }
     document.getElementsByClassName('btn')[0].innerHTML="Start";
@@ -78,76 +95,100 @@ function startStop(){
     document.getElementsByClassName('btn')[1].disabled=false;
     started=false;
   }
-
-}
-
-function reset(){
-  cdTimer= (Number(workDuration.innerHTML) + Number(restDuration.innerHTML))*60000;
-  countDownDate = new Date();
-  countDownDate.setMilliseconds(countDownDate.getMilliseconds()+ cdTimer);
-  adjustSessionDuration();
 }
 
 function myTimer(){
+  animer();
 
-  afficher();
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "FINISHED";
-        emitSound()
+  if ( distance < 0) {
+    clearInterval(x);
+    rdProgress.style.width ="0%";
+    rdProgress.innerHTML="";
+    sessionProgress.style.width ="100%";
+    sessionProgress.innerHTML= (Number(workDuration.innerHTML) + Number(restDuration.innerHTML)) +" m";
+      document.getElementById("timer").classList.remove("awesome");
+      document.getElementById("timer").innerHTML = "FINISHED";
+      document.getElementsByClassName('btn')[0].innerHTML="Start";
+      document.getElementsByClassName('btn')[0].setAttribute('class', 'btn btn-outline-success');
+      document.getElementsByClassName('btn')[1].disabled=false;
+      started=false;
+      setTimeout(function(){ adjustSessionDuration() }, 5000);   
+  }
+  if ( distance - Number(restDuration.innerHTML)*60000 <=3000 && distance - Number(restDuration.innerHTML)*60000 >0) {
+    document.getElementById("timer").classList.add("awesome");
+    setTimeout(function(){ document.getElementById("timer").classList.remove("awesome") }, 8000);
+    emitSound();
+  };
+
+    if (distance <= 3000) {
+        rt="";
         document.getElementById("timer").classList.add("awesome");
-        setTimeout(function(){ document.getElementById("timer").classList.remove("awesome") }, 5000);
-        document.getElementsByClassName('btn')[0].innerHTML="Start";
-        document.getElementsByClassName('btn')[0].setAttribute('class', 'btn btn-outline-success');
-        document.getElementsByClassName('btn')[1].disabled=false;
-        started=false;
-    }
+        emitSound();
+    };
+    
 }
-function afficher(){
-    now = new Date().getTime();
-    distance = countDownDate - now;
 
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    if (hours == 0 ) {
-      document.getElementById("timer").innerHTML =  minutes + " m " + seconds + " s ";
+function CalcTime(){
+  now = new Date().getTime();
+  distance = countDownDate - now;
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.round(((distance) % (1000 * 60)) / 1000);
+
+  if ((hours*60 + minutes) >=60) {
+    wt= (hours*60 + minutes - restDuration.innerHTML) + " m " + seconds + " s";
+   }
+   else if ((hours*60 + minutes) <60){
+    wt= minutes - restDuration.innerHTML + " m " + seconds + " s";
+   };
+}
+
+function afficher(){
+ 
+    if (hours == 0 && minutes == 0) {
+      document.getElementById("timer").innerHTML = seconds + " seconds ";
     }
-    else if (minutes == 0) {
-      document.getElementById("timer").innerHTML = seconds + " s ";
+    else if (hours == 0 ) {
+      document.getElementById("timer").innerHTML =  minutes + " m " + seconds + " s ";
     }
    else {
     document.getElementById("timer").innerHTML = hours + " h " + minutes + " m " + seconds + " s ";
    };
-   sessionProgress.innerHTML= (Number(restDuration.innerHTML) + Number(workDuration.innerHTML))- minutes -1+ " m";
-    if (w>0){
-      wdProgress.innerHTML= minutes - restDuration.innerHTML + " m " + seconds + " s";
-      rdProgress.innerHTML=restDuration.innerHTML + " m";
-      w = w-s;
 
-      if (w>0 && w<3) {
-        emitSound()
-        document.getElementById("timer").classList.add("awesome");
-        setTimeout(function(){ document.getElementById("timer").classList.remove("awesome") }, 5000);
-      }
+   wdProgress.style.width = w +"%";   
+   wdProgress.innerHTML= wt;
+   rdProgress.style.width = r +"%";
+   rdProgress.innerHTML=rt;
+   sessionProgress.style.width = s+ "%";
+   sessionProgress.innerHTML= st;
+  };
+
+function animer(){
+  if ((hours*60 + minutes && s+duree < Number(workDuration.innerHTML)*6000000/cdTimerConst) >=60) {
+    wt= (hours*60 + minutes - restDuration.innerHTML) + " m " + seconds + " s";
+   }
+   else if ((hours*60 + minutes && s+duree < Number(workDuration.innerHTML)*6000000/cdTimerConst) <60){
+    wt= minutes - restDuration.innerHTML + " m " + seconds + " s";
+   };
+
+   CalcTime();
+   st= (Number(restDuration.innerHTML) + Number(workDuration.innerHTML))- minutes-1 + " m " + (60-seconds) + " s";
+   s= s + duree;
+   if (s < Number(workDuration.innerHTML)*6000000/cdTimerConst){
+      rt=restDuration.innerHTML + " m";
+      w = w-duree;
     }
-    else {
-
+    else if (s >= Number(workDuration.innerHTML)*6000000/cdTimerConst) {
+      wt="";
+      r = r+ w - duree;
       w=0;
-      r = r-s;
-      rdProgress.innerHTML= minutes + " m " + seconds + " s";
-    }
-    wdProgress.style.width = w+"%";    
-    rdProgress.style.width = r+"%";
-    sessionProgress.style.width = (100-w-r)+"%"
+      rt= minutes + " m " + seconds + " s";
 
-}
+    };
+    afficher();
+};
+
 function emitSound(){
-  var context = new (window.AudioContext || window.webkitAudioContext)();
-  var osc = context.createOscillator(); // instantiate an oscillator
-  osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
-  osc.frequency.value = 440; // Hz
-  osc.connect(context.destination); // connect it to the destination
-  osc.start(); // start the oscillator
-  osc.stop(context.currentTime + 2); // stop 2 seconds after the current time
+  var beep = document.getElementById("beepAudio"); 
+  beep.play(); 
 }
